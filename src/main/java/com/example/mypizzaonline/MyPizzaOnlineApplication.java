a63221859755392a0b93541095417243c30d8520
deepthi <deepthi.varanasi@iths.se>
2021-08-25 09:59:59 +0200

    Initial commit

---
+++ src/main/java/com/example/mypizzaonline/MyPizzaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.mypizzaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPizzaOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPizzaOnlineApplication.class, args);
+    }
+
+}
